c7da7dd2eb6507ff0ec744da02de8419e787f84b
wujulylife <2118599692@qq.com>
2024-05-21 16:32:42 +0800

    测试

---
+++ test.py
@@ -0,0 +1,7 @@
+#coding=utf-8
+import json
+import requests
+import pandas as pd
+import numpy as np
+data = pd.read_csv('/Users/wushengfan/Library/Mobile Documents/com~apple~CloudDocs/vscode/python/cs-training.csv',index_col=0)
+print(data.head())
